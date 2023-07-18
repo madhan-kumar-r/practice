b72eddfc2ffee3f5510e52976a68a421c9eca1ca
madhan-kumar-r <madhanab29@gmail.com>
2023-07-18 14:39:07 +0530

    removed-comments

--- src/App.js
+++ src/App.js
@@ -25,32 +25,7 @@
    );
  }
 
-// import React, { useReducer } from 'react';
 
-// const initialState = { count: 0 };
-
-// const reducer = (state, action) => {  
-//   switch (action.type) {
-//     case 'INCREMENT':
-//       return { count: state.count + 1 };
-//     case 'DECREMENT':
-//       return { count: state.count - 1 };
-//     default:
-//       return state;
-//   }
-// };
-
-// const Counter = () => {
-//   const [state, dispatch] = useReducer(reducer, initialState);
-
-//   return (
-//     <div>
-//       <p>Count: {state.count}</p>
-//       <button onClick={() => dispatch({ type: 'INCREMENT' })}>Increment</button>
-//       <button onClick={() => dispatch({ type: 'DECREMENT' })}>Decrement</button>
-//     </div>
-//   );
-// };
 
 
 export default App;
